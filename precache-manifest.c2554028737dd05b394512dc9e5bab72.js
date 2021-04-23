e4a528c1dd02c0db4b63781b1af5cb385cb7d84f
Marco <marcobriatore1@gmail.com>
2021-04-23 18:07:45 -0300

    Updates

---
+++ precache-manifest.c2554028737dd05b394512dc9e5bab72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86f64dbbeac33db183da94a5838f76f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6c5dd75b084144459ab9",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f73469c63ee0809c77a7",
+    "url": "/home/static/js/2.2c19a9a7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.2c19a9a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5dd75b084144459ab9",
+    "url": "/home/static/js/main.e7cde279.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "726047db2cad90d4c1be881be628bd76",
+    "url": "/home/static/media/hashirshoaeb.726047db.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
